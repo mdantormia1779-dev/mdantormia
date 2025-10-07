85a323565d0b2ce27bbbaa9583b42a2d59584d01
md antor mia <mdantormia1779@gmail.com>
2025-10-07 23:11:37 +0600

    some update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import Header from './Header/Header'
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Header/Body/Home';
-import All from './All';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './Header/Body/Home'
+import All from './All'
 import ErrorPage from './ErrorPage'
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/all" element={<All />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/all" element={<All />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   )
 }
 
